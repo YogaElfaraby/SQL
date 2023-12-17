9e9034ffa3114665eb2e3479acdf2c6176e63943
Arzaq Ajradika <arzaqajradika1@gmail.com>
2023-12-17 17:50:43 +0700

    fix

--- resources/views/dashboard/AddSchedule.blade.php
+++ resources/views/dashboard/AddSchedule.blade.php
@@ -113,6 +113,6 @@
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="script.js"></script>
+    <script src="assets/js/script.js"></script>
 </body>
 </html>
